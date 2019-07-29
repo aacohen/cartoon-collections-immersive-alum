26bb9098e1e7294e808124c94440ce978a1949de
Ariela Cohen <arielaades@gmail.com>
2019-07-29 18:58:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.map do |value|
-    value.length > 4
+  array.any? {|value| value.length > 4}
+  
   end
 end
 
