c1aa773e3937de92e89bd71149dd4d839928b365
Ariela Cohen <arielaades@gmail.com>
2019-07-29 18:52:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.find do |value|
+  array.map do |value|
     value.length > 4
   end
 end
