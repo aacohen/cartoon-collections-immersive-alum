5fe05b010c6a0d88862f7c54e345239b283093bc
Ariela Cohen <arielaades@gmail.com>
2019-07-29 18:46:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,13 @@
 def roll_call_dwarves(array)# code an argument here
   array.each_with_index do |value, index|
   puts "#{index+1} #{value}"
-end
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+  array.collect do |element|
+    element.first.upcase << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
